e03b336d1afe0f7551865b3901e1fb7bf9a57e64
Subramaniyam Raizada <sub@subraizada.com>
2020-11-16 01:19:24 -0600

    Work around heders which disallow iframe X-Frame-Options header can safely be removed. However the Content-Security-Policy header specifies a number of security options, and aside from the "frame-ancestors" part, most of this header should be kept. The current approach is to just delete the entire CSP header, which is not ideal. Should update this to just remove the frame-ancestors part of the header value.

--- background.js
+++ background.js
@@ -46,3 +46,38 @@ browser.webRequest.onBeforeRequest.addListener(
     {urls: [pattern]},
     ["blocking"]
 );
+
+
+
+// https://usamaejaz.com/bypassing-security-iframe-webextension/
+browser.webRequest.onHeadersReceived.addListener(info => {
+    const headers = info.responseHeaders; // original headers
+    for (let i=headers.length-1; i>=0; --i) {
+        let header = headers[i].name.toLowerCase();
+        if (header === "x-frame-options" || header === "frame-options") {
+            headers.splice(i, 1); // Remove the header
+        }
+    }
+    // return modified headers
+    return {responseHeaders: headers};
+}, {
+    urls: [ "<all_urls>" ], // match all pages
+    types: [ "sub_frame" ] // for framing only
+}, ["blocking", "responseHeaders"]);
+
+browser.webRequest.onHeadersReceived.addListener(info => {
+    const headers = info.responseHeaders; // original headers
+    for (let i=headers.length-1; i>=0; --i) {
+        let header = headers[i].name.toLowerCase();
+        if (header === "content-security-policy") { // csp header is found
+            // modifying frame-ancestors; this implies that the directive is already present
+            //headers[i].value = headers[i].value.replace("frame-ancestors", "frame-ancestors https://yourpage.com/");
+            headers.splice(i, 1); // Remove the header
+        }
+    }
+    // return modified headers
+    return {responseHeaders: headers};
+}, {
+    urls: [ "<all_urls>" ], // match all pages
+    types: [ "sub_frame" ] // for framing only
+}, ["blocking", "responseHeaders"]);
