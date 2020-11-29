29f9ac149ff624ff3762695436ba36d4027eee84
Brendan Moore <moorebrendanr@gmail.com>
2020-11-29 17:12:01 -0500

    Can now drag iframe

--- openIFrame.js
+++ openIFrame.js
@@ -7,18 +7,27 @@ function onReceived(message) {
         console.log("Creating iframe");
         let div = document.createElement("div");
         div.id = "linkPreviewContainer";
+
         let iframe = document.createElement("iframe");
         iframe.src = message.url;
         iframe.id = "linkPreviewIframe";
-        div.appendChild(iframe);
+
+        let headerBar = document.createElement("div");
+        headerBar.id = "headerBar";
+
         let btn = document.createElement("button");
         btn.type = "button";
         btn.id = "iframeCloseBtn";
         btn.innerHTML = "x";
         btn.onclick = function() { $("#linkPreviewContainer").remove() }
-        div.appendChild(btn);
+
+        headerBar.appendChild(btn);
+        div.appendChild(headerBar);
+        div.appendChild(iframe);
         document.body.appendChild(div);
+
         setResizable();
+        dragElement(document.getElementById("linkPreviewContainer"));
     }
 }
 
@@ -62,8 +71,53 @@ function setResizable() {
             $("#iframe-barrier", ui.element).remove();
         },
         resize: function(event, ui) {
-            $("iframe", ui.element).width(ui.size.width).height(ui.size.height);
+            $("iframe", ui.element).width(ui.size.width).height(ui.size.height - 20); // leave room for the header bar
         }
     });
 }
+
+// https://www.w3schools.com/howto/howto_js_draggable.asp
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById("headerBar")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById("headerBar").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    console.log("header clicked");
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 setResizable();
+dragElement(document.getElementById("linkPreviewContainer"));
