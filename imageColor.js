4289774e144421cba0cb72296e1df720b8615e52
Subramaniyam Raizada <sub@subraizada.com>
2020-12-16 09:53:24 -0500

    move getAvgColor function into separate file so it can be shared between background and content scripts

---
+++ imageColor.js
@@ -0,0 +1,34 @@
+function getAvgColor(imageUrl) {
+    return new Promise((resolve) => {
+        let canvas = document.createElement("canvas");
+        let ctx = canvas.getContext("2d");
+        let htmlImage = new Image();
+        htmlImage.onload = function () {
+            console.log("Image loaded");
+            canvas.width = htmlImage.width;
+            canvas.height = htmlImage.height;
+            ctx.drawImage(htmlImage, 0, 0);
+            // document.body.appendChild(canvas);
+            let imageData = ctx.getImageData(0, 0, htmlImage.width, htmlImage.height);
+            let r = 0, g = 0, b = 0, a = 0;
+            for (let i = 0; i < imageData.data.length; i += 4) {
+                r += imageData.data[i];
+                g += imageData.data[i+1];
+                b += imageData.data[i+2];
+                a += imageData.data[i+3];
+            }
+            let divisor = imageData.data.length / 4;
+            r /= divisor;
+            g /= divisor;
+            b /= divisor;
+            a /= divisor;
+            resolve({
+                r: r,
+                g: g,
+                b: b,
+                a: a
+            });
+        };
+        htmlImage.src = imageUrl;
+    });
+}
--- openIFrame.js
+++ openIFrame.js
@@ -326,38 +326,3 @@ function insertAfter(newNode, referenceNode) {
 function getFaviconURL(domainURL) {
     return "https://s2.googleusercontent.com/s2/favicons?domain_url=" + domainURL;
 }
-
-function getAvgColor(imageUrl) {
-    return new Promise((resolve) => {
-        let canvas = document.createElement("canvas");
-        let ctx = canvas.getContext("2d");
-        let htmlImage = new Image();
-        htmlImage.onload = function () {
-            console.log("Image loaded");
-            canvas.width = htmlImage.width;
-            canvas.height = htmlImage.height;
-            ctx.drawImage(htmlImage, 0, 0);
-            // document.body.appendChild(canvas);
-            let imageData = ctx.getImageData(0, 0, htmlImage.width, htmlImage.height);
-            let r = 0, g = 0, b = 0, a = 0;
-            for (let i = 0; i < imageData.data.length; i += 4) {
-                r += imageData.data[i];
-                g += imageData.data[i+1];
-                b += imageData.data[i+2];
-                a += imageData.data[i+3];
-            }
-            let divisor = imageData.data.length / 4;
-            r /= divisor;
-            g /= divisor;
-            b /= divisor;
-            a /= divisor;
-            resolve({
-                r: r,
-                g: g,
-                b: b,
-                a: a
-            });
-        };
-        htmlImage.src = imageUrl;
-    });
-}
