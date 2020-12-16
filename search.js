7700744a39359f00f5e1670e2fd9008091bec0c4
Subramaniyam Raizada <sub@subraizada.com>
2020-12-16 10:42:15 -0500

    copy 2d4836f to search.js

--- search.js
+++ search.js
@@ -181,7 +181,11 @@ function populateResults(saves) {
         p.appendChild(document.createTextNode(save.url));
         p.appendChild(document.createElement('br'));
         p.appendChild(document.createElement('br'));
-        p.appendChild(document.createTextNode(`Saved ${save.date.toString()}`));
+        p.appendChild(document.createTextNode(`Saved ${dateToString(save.saveDate)}`));
+        p.appendChild(document.createElement('br'));
+        p.appendChild(document.createTextNode(`Created ${dateToString(save.createdDate)}`));
+        p.appendChild(document.createElement('br'));
+        p.appendChild(document.createTextNode(`Modified ${dateToString(save.updateDate)}`));
         p.appendChild(document.createElement('br'));
         if (save.numNodes === 1)
             p.appendChild(document.createTextNode('1 page'));
